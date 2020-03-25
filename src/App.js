7b907cba1184509fc15c0424b92aaf2f5cf1017f
jorgepazp <paz.jorge1@gmail.com>
2020-03-25 17:27:46 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react';
+import MaterialDatatable from "material-datatable";
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
-function App() {
+
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const [data, setData] = useState([]);
+  const onSubmit = data =>{ 
+    console.log(data);
+    axios.post('http://localhost:8000/persona',data)
+    .then(res=>{
+      console.log(res)
+      cargar();
+    })
+  }
+
+  const cargar = () =>{
+    axios.get('http://localhost:8000/personas')
+    .then(res =>{
+      setData(res.data.personas);
+    })
+  }
+
+  useEffect(() => {
+    console.log('TODAS LAS PERSONAS:');
+
+    console.log(data);
+  }, [data]);
+  
+  const columns = [{
+      name: 'Nombre',
+      field: 'nombre',
+      options: {
+        filter: true,
+        sort: true
+      }
+    },
+    {
+      name: 'Apellido',
+      field: 'apellido',
+      options: {
+        filter: true,
+        sort: false
+      }
+    }
+  ];
+
+  const options = {
+    filterType: 'checkbox'
+  };
+  const datas = data.map(personas => ({
+    value: personas._id,
+    nombre: personas.nombre,
+    apellido: personas.apellido
+  }));
+
+
+  useEffect(() => {
+    cargar();
+  },[]);
+  
+  console.log(errors);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="nombre" name="nombre" ref={register} />
+      <input type="text" placeholder="apellido" name="apellido" ref={register} />
+      
+      <input type="submit" />
+      <MaterialDatatable
+        title={'Personas'}
+        data={datas}
+        columns={columns}
+        options={options}
+      />
+
+    </form>
   );
 }
-
-export default App;
